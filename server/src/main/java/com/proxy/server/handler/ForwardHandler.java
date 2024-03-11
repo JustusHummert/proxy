5737074cf87b707ded30a1f865db6888070a11de
Justus Hummert <justusjhummert@aol.com>
2024-03-11 17:48:52 +0100

    remove changing the answer

--- server/src/main/java/com/proxy/server/handler/ForwardHandler.java
+++ server/src/main/java/com/proxy/server/handler/ForwardHandler.java
@@ -2,7 +2,6 @@ package com.proxy.server.handler;
 
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,8 +14,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
+import java.net.URI;
 import java.util.List;
-import java.util.function.Consumer;
+
 
 public class ForwardHandler {
 
@@ -45,10 +45,6 @@ public class ForwardHandler {
                 .retrieve()
                 .toEntity(byte[].class)
                 .flatMap(responseEntity -> {
-                    MediaType contentType = responseEntity.getHeaders().getContentType();
-                    byte[] body = responseEntity.getBody();
-                    if(contentType != null && contentType.toString().contains("text"))
-                        body = modifyAnswer(body, url, "https://" + request.getServerName());
                     //Get the Cookies from the response and store it in the session
                     List<String> cookiesList = responseEntity.getHeaders().get(HttpHeaders.SET_COOKIE);
                     if (cookiesList != null)
@@ -57,16 +53,16 @@ public class ForwardHandler {
                             String[] cookieNameValue = cookieParts[0].split("=");
                             cookiesMap.add(cookieNameValue[0], cookieNameValue[1]);
                         }
+                    //Create the headers
                     HttpHeaders headers = new HttpHeaders();
-                    headers.setContentType(contentType);
+                    headers.setContentType(responseEntity.getHeaders().getContentType());
                     headers.setLocation(responseEntity.getHeaders().getLocation());
                     return Mono.just(ResponseEntity
                             .status(responseEntity.getStatusCode())
                             .headers(headers)
-                            .body(body));
+                            .body(responseEntity.getBody()));
                 });
     }
-
     //create WebClient
     private static WebClient createWebClient(String url){
         //Configute custom ExchangeStrategies to avoid buffer limit exception
@@ -80,13 +76,5 @@ public class ForwardHandler {
                 .exchangeStrategies(exchangeStrategies)
                 .baseUrl(url)
                 .build();
-    }
-
-    //modify the Answer to change the urls to the proxy url
-    public static byte[] modifyAnswer(byte[] body, String url, String proxy){
-        String answer = new String(body);
-        //replace the url with the proxy url
-        answer = answer.replaceAll(url,proxy);
-        return answer.getBytes();
     }
 }
